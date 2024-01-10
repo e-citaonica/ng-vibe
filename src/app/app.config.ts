df5b5c1f1135b0fc9f1443a0844a7fb727572d81
andrija-tosic <andonnet@gmail.com>
2024-01-10 20:27:40 +0100

    remove: ngx-socket-io

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { SocketIoModule } from 'ngx-socket-io';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    SocketIoModule.forRoot({ url: 'http://localhost:8079', options: {} })
-      .providers!,
-  ],
+  providers: [provideRouter(routes), provideHttpClient()],
 };
